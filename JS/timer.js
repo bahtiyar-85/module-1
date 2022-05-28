6099c7097713c761c5d57efaea1f7d57169117aa
bahtiyar-85 <b.ilyazov@gmail.com>
2022-05-29 00:23:31 +0600

    fix bug after valid html

--- JS/timer.js
+++ JS/timer.js
@@ -4,18 +4,22 @@ document.addEventListener('DOMContentLoaded', function() {
    
     const timer = config.timerEndDate.split(/\s|\.|\:/);
     let timerId = null;
-    const clockElements = document.getElementsByClassName('timer__clock-item');
     const timerElement = document.querySelector('.timer');
+
+    function addZero(number){
+        if(number <= 9 && number > 0 ) return "0" + number;
+        return number;
+    }
 
     function calcTime(timer) { 
         const deadlineDate = new Date(timer[2], timer[1]-1, timer[0], timer[3], timer[4]);
         const differenceTime = deadlineDate.getTime() - Date.now();
         
         if(differenceTime>0){
-            const days = Math.floor( differenceTime / 1000 / 60 / 60 / 24);
-            const hours = Math.floor( differenceTime / 1000 / 60 / 60 % 24);
-            const minutes = Math.floor( differenceTime / 1000 / 60) % 60;
-            const seconds = Math.floor(differenceTime / 1000) % 60;
+            const days = addZero(Math.floor( differenceTime / 1000 / 60 / 60 / 24));
+            const hours =addZero(Math.floor( differenceTime / 1000 / 60 / 60 % 24));
+            const minutes = addZero(Math.floor( differenceTime / 1000 / 60) % 60);
+            const seconds = addZero(Math.floor(differenceTime / 1000) % 60);
             let clock = {
                 timeOut: false,
                 days,
@@ -34,12 +38,12 @@ document.addEventListener('DOMContentLoaded', function() {
     function countdownTimer() {
         const clock = calcTime(timer);
         if(!clock.timeOut){
-            for(let i=0; i<clockElements.length; i++){
-                if(i===0) clockElements[i].textContent = clock.days;
-                else if(i===2) clockElements[i].textContent = clock.hours;
-                else if(i===4) clockElements[i].textContent = clock.minutes;
-                else if(i===6) clockElements[i].textContent = clock.seconds;
-            }
+            document.querySelector("#days").textContent = clock.days;
+            document.querySelector("#hours").textContent = clock.hours;
+            document.querySelector("#minutes").textContent = clock.minutes;
+            document.querySelector("#seconds").textContent = clock.seconds;
+            timerElement.classList.remove("timer_hide");
+            timerElement.classList.add("timer_show");
         } else {
             clearInterval(timerId);
             timerElement.classList.remove("timer_show");
